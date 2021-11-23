899a045e6fb7fd7f24f50dc437fbd3b158a9d6a9
Luciana Sugeta <lulusugeta@gmail.com>
2021-11-23 09:22:59 -0500

    commit tree eded75f7a2dccc055d2b2e5008af18358660c750

--- src/Clock.js
+++ src/Clock.js
@@ -4,9 +4,19 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const timerID = setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+    // returning a cleanup function
+    // it's called by React: 
+    // 1. after the component re-renders after setting state and before the useEffect callback is called
+    // 2. or before the component is removed from the page if it is no longer being returned by a parent component
+    // render -> useEffect -> setState -> re-render -> cleanup -> useEffect 
+    return function cleanup() {
+      clearInterval(timerID)
+    }
+
   }, []);
 
   return <div>{time.toString()}</div>;
